0c141cce3466ea3c8161cc8648bf1b418584847b
MunsterMc <nael.messaoud06@gmail.com>
2022-10-18 11:30:00 +0200

    Added scan and fixed everything yay

--- scripts/camScan.js
+++ scripts/camScan.js
@@ -1,7 +1,17 @@
-import { html5QrcodeScanner } from '../3rd-party/html5-qrcode.min.js'
 function onScanSuccess(decodedText, decodedResult) {
     console.log(`Code scanned = ${decodedText}`, decodedResult);
+    //Once scanned, prompt to scan new QR Code and show output
+    document.getElementById('outputBox').innerHTML = `<br> Scanned : ${decodedText}`;
+    html5QrcodeScanner.clear().then(_ => {
+        // the UI should be cleared here
+    }).catch(error => {
+        // Could not stop scanning for reasons specified in `error`.
+        // This conditions should ideally not happen.
+        Console.log("Error: " + error);
+    });
+
+    
 }
 var html5QrcodeScanner = new Html5QrcodeScanner(
-    "qr-reader", { fps: 10, qrbox: 250 });
+    "qr-reader", { fps: 5, qrbox: 250 });
 html5QrcodeScanner.render(onScanSuccess);
